0a54722ffb90485611ec610458c886fef5e22e44
RishanBaweja <rbaweja@calpoly.edu>
2025-11-06 22:20:06 -0800

    feat: added a taskbar to switches pages

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -6,7 +6,8 @@ import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 import MyApp from "./MyApp";
 import Form from "./pages/Form.jsx";
 import Table from "./pages/Table.jsx";
-import Home from "./pages/Home.jsx";
+import Layout from "./Layout.jsx";
+// import Home from "./pages/Home.jsx";
 import "./main.css";
 
 // If network connectivity issues, no defined route, user error, etc.
@@ -20,21 +21,21 @@ function RouteError() {
 }
 
 /*
-* This function creates the browswer routing between pages. 
-* MyApp page is current layout (element), however this should later be changed to be some sort of Layout.jsx
-* Layout.jsx should be footer, header, navbar, etc. and will be the standard for all pages
-* errorElement goes to our above RouteError function
-* In children is our pages
-* Currently, Home is our root page. This can be changed later, depending on if we want login
-* In order to add a new page, add a new bracket and follow same formatting. Make sure to import the page.
-*/
+ * This function creates the browswer routing between pages.
+ * MyApp page is current layout (element), however this should later be changed to be some sort of Layout.jsx
+ * Layout.jsx should be footer, header, navbar, etc. and will be the standard for all pages
+ * errorElement goes to our above RouteError function
+ * In children is our pages
+ * Currently, Home is our root page. This can be changed later, depending on if we want login
+ * In order to add a new page, add a new bracket and follow same formatting. Make sure to import the page.
+ */
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MyApp />,
+    element: <Layout />,
     errorElement: <RouteError />,
     children: [
-      { index: true, element: <Home /> },
+      { index: true, element: <div>Welcome home!</div> },
       { path: "form", element: <Form /> },
       { path: "table", element: <Table /> },
     ],
