b925f674731c6301a3e1efeb88f17c221595e0b4
Sbillups0 <sbillups0@me.com>
2025-12-01 17:15:35 -0800

    added switch: env var wasnt loading on build; '

--- packages/react-frontend/src/config.js
+++ packages/react-frontend/src/config.js
@@ -1,4 +1,9 @@
-const API_BASE =
-  import.meta.env.VITE_API_URL || "http://localhost:8000";
+const isHosted =
+  typeof window !== "undefined" &&
+  window.location.hostname.includes("azurestaticapps.net");
+
+const API_BASE = isHosted
+  ? "https://lowballers-efdua2e5h8fsg5bx.westus3-01.azurewebsites.net/"
+  : "http://localhost:8000";
 
 export default API_BASE;
